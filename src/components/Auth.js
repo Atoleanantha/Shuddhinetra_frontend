67e673b2797d541ef9c650ffeb7aacb5b7c81d15
Anantha Atole <atoleanantha03@gmail.com>
2024-12-07 13:46:00 +0530

    login design and bug fixing

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -33,9 +33,9 @@ const Auth = ({ setUser }) => {
         localStorage.setItem('user', JSON.stringify(data.user[0]));
         localStorage.setItem('login', JSON.stringify(data));
         setUser(data.user[0]);
-        
-        console.log("data:",data);
-        
+
+        console.log("data:", data);
+
         showPopup('Login successful', 'success');
         navigate('/dashboard', { replace: true });
       } else {
@@ -50,7 +50,7 @@ const Auth = ({ setUser }) => {
   };
 
 
-  
+
   const handleRegister = async () => {
     if (password !== confirmPassword) {
       showPopup('Passwords do not match', 'error');
@@ -62,7 +62,7 @@ const Auth = ({ setUser }) => {
       const newUser = {
         username,
         password,
-        confirm_password:confirmPassword,
+        confirm_password: confirmPassword,
         full_name: fullName,
         phone_number: phoneNumber,
         email,
@@ -80,7 +80,7 @@ const Auth = ({ setUser }) => {
 
       console.log("register:", data.token);
       setToken(data.token)
-      localStorage.setItem('authToken',data.token)
+      localStorage.setItem('authToken', data.token)
       setUser(data.user[0])
       showPopup('Registration successful', 'success');
       navigate('/dashboard');
@@ -108,9 +108,15 @@ const Auth = ({ setUser }) => {
   const [showForgotPassword, setShowForgotPassword] = useState(false);
 
   return (
+
     <Box
       sx={{
-        backgroundColor: '#9A616D',
+        backgroundImage: 'url(/16922846_5820617.jpg)',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+
+
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
@@ -128,17 +134,96 @@ const Auth = ({ setUser }) => {
                     <Grid container>
                       <Grid item xs={12} sm={6}>
                         <img
-                          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+                          src="https://thumbs.dreamstime.com/b/prakash-indian-postman-223377163.jpg"
                           alt="login form"
                           className="img-fluid"
-                          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+
+                          style={{
+                            width: '410px', // Specify your desired width
+                            height: '600px', // Specify your desired height
+                            objectFit: 'cover', // Crops to fill the container while maintaining the aspect ratio
+                            objectPosition: 'left', // Center the cropped image
+                            top: '590px',
+
+                            // borderRadius: '10px', // Optional: Add rounded corners
+                            //boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', // Optional: Add shadow for a 3D effect
+                          }}
                         />
+
+
                       </Grid>
                       <Grid item xs={12} sm={6} sx={{ p: 4, width: '100%' }}>
-                        <Typography variant="h4" fontWeight="bold" sx={{ mb: 3 }}>
+                        <img
+                          src="https://static.vecteezy.com/system/resources/previews/048/958/438/non_2x/security-camera-isolated-on-white-backdrop-graphic-illustration-surveillance-equipment-concept-of-modern-security-technology-monitoring-protection-print-logo-sign-design-element-vector.jpg"
+                          alt="Security Camera"
+                          style={{
+                            position: 'absolute',
+                            top: '0%',
+
+                            left: '92%',
+                            transform: 'translateX(-50%)',
+                            zIndex: 10, // Ensures the image is above the card
+                            width: '200px', // Adjust as needed
+                            height: 'auto',
+
+                          }}
+                        />
+                        <img
+                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg-L2VWHT70GAAwcKtig0cAgde97UchNacHQ&s"
+                          alt="Security Camera"
+                          style={{
+                            position: 'absolute',
+                            bottom: '0%',
+
+                            left: '95%',
+                            transform: 'translateX(-50%)',
+                            zIndex: 11, // Ensures the image is above the card
+                            width: '70px', // Adjust as needed
+                            height: 'auto',
+
+                          }}
+                        />
+
+                        <img
+                          src={`${process.env.PUBLIC_URL}/clean_staff.jpg`}
+                          alt="Security Camera"
+                          style={{
+                            position: 'absolute',
+                            bottom: '0%',
+                            //top:'10%',
+
+                            left: '80%',
+                            transform: 'translateX(-50%)',
+                            zIndex: 3, // Ensures the image is above the card
+                            width: '220px', // Adjust as needed
+                            height: 'auto',
+
+                          }}
+                        />
+
+                        <img
+                          src="https://www.shutterstock.com/shutterstock/videos/1087463630/thumb/7.jpg?ip=x480://thumbs.dreamstime.com/z/d-chart-icon-arrow-point-up-green-bar-d-chart-icon-arrow-point-up-green-bar-141857425.jpg"
+                          alt="Security Camera"
+                          style={{
+                            position: 'absolute',
+                            bottom: '0%',
+                            //top:'10%',
+
+                            left: '54%',
+                            transform: 'translateX(-50%)',
+                            zIndex: 1, // Ensures the image is above the card
+                            width: '270px', // Adjust as needed
+                            height: 'auto',
+
+                          }}
+                        />
+
+                       
+
+                        <Typography variant="h4" fontWeight="bold" color="error" sx={{ mb: 3 }}>
                           Login
                         </Typography>
-                        <TextField
+                        <TextField sx={{ zIndex: 11 }}
                           fullWidth
                           label="Username"
                           variant="outlined"
@@ -156,20 +241,20 @@ const Auth = ({ setUser }) => {
                         <Button
                           onClick={signIn}
                           variant="contained"
-                          color="primary"
+                          color="error" // Uses Material-UI's predefined red
                           size="large"
                           fullWidth
-                          sx={{ mt: 2 }}
+                          sx={{ mt: 2, zIndex: 11 }}
                           disabled={loading}
                         >
                           {!loading ? 'Login' : 'Loading...'}
                         </Button>
-                        <Typography variant="body2" sx={{ mt: 2 }}>
-                          <Link href="#" underline="hover" onClick={handleForgotPassword}>
+                        <Typography variant="body2" sx={{ mt: 2, zIndex: 11 }}>
+                          <Link href="#" underline="hover" onClick={handleForgotPassword} sx={{ zIndex: 11 }}>
                             Forgot password?
                           </Link>
                         </Typography>
-                        <Typography variant="body2" sx={{ mt: 3, color: '#393f81' }}>
+                        <Typography variant="body2" sx={{ mt: 3, color: '#393f81', zIndex: 11 }}>
                           Don't have an account?{' '}
                           <Link href="#" underline="hover" sx={{ color: '#393f81' }} onClick={handleFlip}>
                             Register here
@@ -186,19 +271,85 @@ const Auth = ({ setUser }) => {
                     <Grid container>
                       <Grid item xs={12} sm={6}>
                         <img
-                          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
-                          alt="register form"
+                          // src=".\assets\IMG_20241206_100801.jpg"
+
+                          src={`${process.env.PUBLIC_URL}/IMG_20241206_112647 (1).jpg`}
+                          alt="login form"
                           className="img-fluid"
-                          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                          style={{
+                            width: '450px', // Specify your desired width
+                            height: '700px', // Specify your desired height
+                            objectFit: 'cover', // Crops to fill the container while maintaining the aspect ratio
+                            objectPosition: 'left', // Center the cropped image
+                            left: '300px',
+                            bottom: '100px',
+                            // borderRadius: '10px', // Optional: Add rounded corners
+                            // boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', // Optional: Add shadow for a 3D effect
+                          }}
                         />
+
                       </Grid>
                       <Grid item xs={12} sm={6} md={6} sx={{ p: 4, width: '100%' }}>
-                        <Typography variant="h4" fontWeight="bold" sx={{ mb: 2 }}>
+                        <img
+                          src="https://static.vecteezy.com/system/resources/previews/048/958/438/non_2x/security-camera-isolated-on-white-backdrop-graphic-illustration-surveillance-equipment-concept-of-modern-security-technology-monitoring-protection-print-logo-sign-design-element-vector.jpg"
+                          alt="Security Camera"
+                          style={{
+                            position: 'absolute',
+                            top: '0%',
+
+                            left: '92%',
+                            transform: 'translateX(-50%)',
+                            zIndex: 10, // Ensures the image is above the card
+                            width: '200px', // Adjust as needed
+                            height: 'auto',
+
+                          }}
+                        />
+
+                        <img
+                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg-L2VWHT70GAAwcKtig0cAgde97UchNacHQ&s"
+                          alt="Security Camera"
+                          style={{
+                            position: 'absolute',
+                            bottom: '0%',
+
+                            left: '95%',
+                            transform: 'translateX(-50%)',
+                            zIndex: 11, // Ensures the image is above the card
+                            width: '70px', // Adjust as needed
+                            height: 'auto',
+
+                          }}
+                        />
+
+                        
+                        <img
+                          src={`${process.env.PUBLIC_URL}/Shuddhi_netra_logo.jpg`}
+                          alt="Security Camera"
+                          style={{
+                            position: 'absolute',
+                            bottom: '1%',
+                            //top:'10%',
+
+                            left: '38%',
+                            transform: 'translateX(-50%)',
+                            zIndex: 3, // Ensures the image is above the card
+                            width: '220px', // Adjust as needed
+                            height: 'auto',
+
+                          }}
+                        />
+
+                        
+
+
+                        <Typography variant="h4" fontWeight="bold" color="error" sx={{ mb: 2, zIndex: 9 }}>
                           Register
                         </Typography>
-                        <TextField
+                        <TextField sx={{ zIndex: 12 }}
                           fullWidth
                           label="Full Name"
+
                           variant="outlined"
                           margin="dense"
                           onChange={(e) => setFullName(e.target.value)}
@@ -261,7 +412,7 @@ const Auth = ({ setUser }) => {
                         >
                           <FormControlLabel
                             value="Sub-Divisional"
-                            control={<Radio />}
+                            control={<Radio color="error" />}
                             label="Sub-Divisional Office"
                           />
                         </RadioGroup>
@@ -275,9 +426,9 @@ const Auth = ({ setUser }) => {
                           />
                         )}
                         <Button
-                          onClick={handleRegister}
+                          onClick={signIn}
                           variant="contained"
-                          color="primary"
+                          color="error" // Uses Material-UI's predefined red
                           size="large"
                           fullWidth
                           sx={{ mt: 2 }}
@@ -315,3 +466,9 @@ const Auth = ({ setUser }) => {
 };
 
 export default Auth;
+
+
+
+
+
+
